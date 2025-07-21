6e803ca14f6fbe6d0cad787fbd6ba4e64c5e56c6
FCordeiro <162184965+fecordeiro98@users.noreply.github.com>
2025-07-20 21:08:20 -0300

    grande atualização

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const texto = document.getElementById('texto');
+
 function calcular() {
     let n1 = parseFloat(document.getElementById('n1').value);
     let n2 = parseFloat(document.getElementById('n2').value);
     let resultado = 18 - n1 - n2;
     document.getElementById('resultado').value = resultado.toFixed(1);
+    
+    if (n1 < 4 || n2 < 4) {
+        texto.textContent = 'Uma das notas é menor do que 4, o estudante irá para a reposição!';
+        texto.style.color = 'orange';
+        texto.style.fontWeight = 'bold';
+        document.getElementById('resultado').value = resultado.toFixed(1);
+        return;
+    }
+
+    if (resultado < 4) {
+        document.getElementById('resultado').value = 4;
+        texto.textContent = 'O estudante precisa de menos de 4 pontos, mas precisa tirar pelo menos 4 pontos para não ir para a reposição!';
+        texto.style.color = '#164194';
+        texto.style.fontWeight = 'bold';
+    } else {
+        document.getElementById('resultado').value = resultado.toFixed(1);
+        texto.textContent = '';
+    }
 }
